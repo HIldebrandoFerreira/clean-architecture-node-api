7d33d15e363d8f897254dbbd6cfb02c7ba229eda
Hildebrando Ferreira <hildebrandoweb@gmail.com>
2025-03-18 21:44:29 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,16 @@
 export class SignUpController {
   handle(httreqest: any): any {
-    return {
-      statusCode: 400,
-    };
+    if (!httreqest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: name"),
+      };
+    }
+    if (!httreqest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: email"),
+      };
+    }
   }
 }
