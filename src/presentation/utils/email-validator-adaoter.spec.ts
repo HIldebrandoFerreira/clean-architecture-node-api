3ccc4b568c365049636f6e6e9d4dcccae70d01fe
Hildebrando Ferreira <hildebrandoweb@gmail.com>
2025-03-31 22:48:34 -0300

    feat: ensure EmailValidatorAdapter return true if validator returns true

--- src/presentation/utils/email-validator-adaoter.spec.ts
+++ src/presentation/utils/email-validator-adaoter.spec.ts
@@ -1,9 +1,21 @@
 import { EmailValidatorAdapter } from "./email-validator";
 
+jest.mock("validator", () => ({
+  isEmail(): boolean {
+    return true;
+  },
+}));
+
 describe("Email Validator Adapter", () => {
   test("Should return false if validator returns false", () => {
     const sut = new EmailValidatorAdapter();
-    const isValid = sut.isValid("invalid_email@gmailcom");
+    jest.spyOn(sut, "isValid").mockReturnValueOnce(false);
+    const isValid = sut.isValid("invalid_email@gmail.com");
     expect(isValid).toBe(false);
+  });
+  test("Should return true if validator returns true", () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid("invalid_email@gmail.com");
+    expect(isValid).toBe(true);
   });
 });
