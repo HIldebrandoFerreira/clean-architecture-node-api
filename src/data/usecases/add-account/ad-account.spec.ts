cabe6e2de122c13cb9e3c6753651adeaa4f53947
Hildebrando Ferreira <hildebrandoweb@gmail.com>
2025-04-02 14:31:49 -0300

    refactor: ensure move to DBAddAccount and Encrypter for makeSut

--- src/data/usecases/add-account/ad-account.spec.ts
+++ src/data/usecases/add-account/ad-account.spec.ts
@@ -1,14 +1,28 @@
+import { Encrypter } from "../../protocols/encrypter";
 import { DBAddAccount } from "./db-ad-account";
+
+interface SutTypes {
+  sut: DBAddAccount;
+  encrypterdStub: Encrypter;
+}
+
+const makeEncrypterStub = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_password"));
+    }
+  }
+  return new EncrypterStub();
+};
+const makeSut = (): SutTypes => {
+  const encrypterdStub = makeEncrypterStub();
+  const sut = new DBAddAccount(encrypterdStub);
+  return { sut, encrypterdStub };
+};
 
 describe("DBAddAccount UseCase", () => {
   test("Should call Encrypter with correct password", async () => {
-    class EncrypterStub {
-      async encrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve("hashed_password"));
-      }
-    }
-    const encrypterdStub = new EncrypterStub();
-    const sut = new DBAddAccount(encrypterdStub);
+    const { sut, encrypterdStub } = makeSut();
     const encryptSpy = jest.spyOn(encrypterdStub, "encrypt");
     const accountData = {
       name: "valid_name",
