017d6234e1c05cfc676c6c9977766d35fa1348cb
Hildebrando Ferreira <hildebrandoweb@gmail.com>
2025-04-08 23:14:38 -0300

    chore: add jestcongif @shelf/jest-mongodb

---
+++ jest-mongodb-config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  mongodbMemoreyServerOptions: {
+    instance: {
+      dbName: "jest",
+    },
+    Binary: {
+      version: "4.0.3",
+      skipMD5: true,
+    },
+    autoStart: false,
+  },
+};
